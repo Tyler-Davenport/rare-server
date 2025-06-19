c58fb88c894f6ab473460e0bb5556afc6e762018
Ozzy Akben <akbenof@gmail.com>
2025-06-18 22:24:06 -0400

    refactor: reorganize imports and improve comment retrieval logic

--- rareapi/views/comment.py
+++ rareapi/views/comment.py
@@ -1,18 +1,22 @@
-from rest_framework.viewsets import ViewSet
-from rest_framework.response import Response
+from rareapi.models import Comment, Post
+from rareapi.models.user import User
+from rareapi.serializers import CommentSerializer
 from rest_framework import serializers, status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rareapi.models import Comment
-from rareapi.serializers import CommentSerializer
-from rareapi.models.user import User
-from rareapi.models import Post
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+
 
 class CommentViewSet(ViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def list(self, request):
-        """Handle GET requests to retrieve all comments"""
-        comments = Comment.objects.all()
+        """Handle GET requests to retrieve all comments or filter by post_id"""
+        post_id = request.query_params.get("post_id", None)
+        if post_id:
+            comments = Comment.objects.filter(post_id=post_id)
+        else:
+            comments = Comment.objects.all()
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -25,24 +29,30 @@ class CommentViewSet(ViewSet):
         serializer = CommentSerializer(data=request.data)
         if serializer.is_valid():
             try:
-                author = User.objects.get(pk=serializer.validated_data['author_id'])
-                post = Post.objects.get(pk=serializer.validated_data['post_id'])
+                author = User.objects.get(pk=serializer.validated_data["author_id"])
+                post = Post.objects.get(pk=serializer.validated_data["post_id"])
                 comment = Comment.objects.create(
                     author=author,
                     post=post,
-                    content=serializer.validated_data['content']
+                    content=serializer.validated_data["content"],
                 )
-                return Response(CommentSerializer(comment).data, status=status.HTTP_201_CREATED)
+                return Response(
+                    CommentSerializer(comment).data, status=status.HTTP_201_CREATED
+                )
             except User.DoesNotExist:
-                return Response({"error": "Author not found."}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {"error": "Author not found."}, status=status.HTTP_400_BAD_REQUEST
+                )
             except Post.DoesNotExist:
-                return Response({"error": "Post not found."}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {"error": "Post not found."}, status=status.HTTP_400_BAD_REQUEST
+                )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def update(self, request, pk=None):
         # TODO: Add check to ensure request.user is the author of the comment
         comment = Comment.objects.get(pk=pk)
-        comment.content = request.data.get('content', comment.content)
+        comment.content = request.data.get("content", comment.content)
         comment.save()
         return Response(CommentSerializer(comment).data, status=status.HTTP_200_OK)
 
@@ -51,4 +61,3 @@ class CommentViewSet(ViewSet):
         comment = Comment.objects.get(pk=pk)
         comment.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
